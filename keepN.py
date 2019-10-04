3054a33bda92cc26c3b06ee711998eb2018d5cbc
Orlando Soto <oj.5070@gmail.com>
2019-10-04 11:19:35 -0400

    added perpetual running

---
+++ keepN.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+import glob
+import os
+from sys import argv
+import subprocess
+import shlex
+from subprocess import Popen, PIPE
+from time import sleep
+
+############## global setting ###########
+outdir=""
+outdir_temp="/volatile/clas/claseg2/osoto/sim/hipo/H500"
+app="/home/osoto/clasdis-nocernlib/clasdis"
+script="run_chain.csh"
+gcard="/group/clas12/gemc/gcards/rga-fall2018.gcard"
+WF="clas12sim_cont"
+curr_status_file = "/home/osoto/sim_status.txt"
+MAXJOBS = 100
+DEBUG=False
+#################################################
+##### add job to workflow
+def add_job(wf,c=0):
+    global outdir, app, script
+    jname = wf + "_" + str(c)
+    cmd = "swif add-job -workflow " + wf + " -ram 2000mb -project clas12 -track simulation -disk 8000mb "
+    cmd = cmd + " -name " + jname
+    cmd = cmd + " -output skimmed.hipo " + outdir + "/" + "skimmed_" + str(c) + ".hipo"
+    cmd = cmd + " -input " + app.split("/")[-1] + " file:" + app
+    cmd = cmd + " -input " + gcard.split("/")[-1] + " file:" + gcard
+    cmd = cmd + " -script " + script
+    if DEBUG : print (cmd)
+    subprocess.call(cmd,shell=True)
+
+######### create workflow #####
+def create_wf():
+    global WF
+    cmd="swif create " + WF
+    if DEBUG : print (cmd)
+    subprocess.call(cmd,shell=True)
+
+#### run workflow ############
+def run_wf():
+    global WF
+    cmd="swif run -workflow " + WF
+    subprocess.check_call(cmd.split(" "))
+
+#### check running jobs ############
+def check_wf():
+    global WF
+    states = []
+    problems = []
+    jobs = []
+    cmd='swif status "' + WF +  '" -jobs' 
+    ret = Popen(shlex.split(cmd),stdout=PIPE)
+    out,err = ret.communicate()
+    out = out.strip()
+    for l in out.split('\n'):
+        if "id" in l.split():
+            jobid = l.split()[-1]
+            if jobid not in jobs:
+                jobs.append(jobid)
+        if "auger_current_state" in l.split():
+            states.append(l.split()[-1])
+        if "problem" in l.split():
+            problems.append(l.split()[-1])
+
+    act = states.count('ACTIVE')
+    pend = states.count('PENDING')
+    succ = states.count('DONE')
+    fail = len(problems)
+    total = len(jobs)
+    return (act,pend,succ,fail,total)
+
+######## main routine ####
+
+def main():
+    global WF, outdir, outdir_temp, MAXJOBS
+    create_wf()
+    run_wf()
+    ret = Popen(shlex.split("cat " + curr_status_file),stdout=PIPE)
+    out, err = ret.communicate()
+
+    bunch =  int(out.strip().split('\n')[-1].split('/')[0])
+    job_i =  int(out.strip().split('\n')[-1].split('/')[1])
+    outdir = outdir_temp + "_" + str(bunch)
+    cmd = "mkdir -p " + outdir
+    print cmd
+    subprocess.call(cmd,shell=True)
+
+    while True:
+        if job_i==500:
+            bunch += 1
+            job_i = 0
+            outdir = outdir_temp + "_" + str(bunch)
+            cmd = "mkdir -p " + outdir
+            print cmd
+            subprocess.call(cmd,shell=True)
+
+
+        act,pend,succ,fail,total = check_wf()
+
+        print 'T / A / P / C / F'
+        print str(total) + ' / ' + str(act) + ' / ' + str(pend) + ' / ' + str(succ) + ' / ' + str(fail)
+        
+        add_job(WF,job_i)
+        while (total - succ - fail) > MAXJOBS:
+            sleep(10)
+        job_i +=1
+        fout = open(curr_status_file,"w")
+        fout.write("Bunch / job_i\n")
+        fout.write(str(bunch) + " / " + str(job_i))
+        fout.close()
+        sleep(1)
+        
+    
+if __name__=="__main__":
+    main()
--- run_chain.csh
+++ run_chain.csh
@@ -1,4 +1,5 @@
-#!/bin/csh
+#!/bin/csh -f
+source ~/.cshrc
 mkdir eventfiles
 ./clasdis --trig 10000 --nmax 10000 --zpos -3.0 --zwidth 5 --raster 0 --w 3.92 20 --q 0.85 15
 cat eventfiles/*.dat > lundFile.dat
@@ -6,3 +7,4 @@ setenv fname `basename eventfiles/*.dat`
 gemc rga-fall2018.gcard -INPUT_GEN_FILE="LUND, lundFile.dat" -OUTPUT="evio, out.evio" -RUNNO=11 -USE_GUI=0 -N=10000;
 $COATJAVA/bin/evio2hipo -r 11 -t -1.0 -s -1.0 -o output.hipo out.evio;
 recon-util -i output.hipo -o cooked.hipo -c 2 -y /group/clas12/gemc/gcards/rga-fall2018.yaml;
+$CLARA_HOME/plugins/clas12/bin/hipo-utils -filter -b "RUN*,MC*,REC*" -o skimmed.hipo cooked.hipo
